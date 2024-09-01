4b945a627d0a01526004a0074b6cc0623b8b5e6c
Roxana Danalache <roxydanalache17@gmail.com>
2024-09-01 21:28:21 +0300

    commit initial

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-console.log("test");
+const players = [
+    {name: "Ted", points:500},
+    {name: "Ron", points:200},
+    {name: "Ben", points:300},
+];
+const top = players
+ .sort ((p1, p2) => p1.points + p2.points)
+ .map ((p) => p.name + " are punctajul de" + p.points);
+
+console.log(top);
+
